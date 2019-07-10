5a615114b64479153a2e15f768d24b4ac7a451fa
Damien Jacinto <damien.jacinto@gmail.com>
2019-07-10 23:41:31 +0200

    Add test on counter

---
+++ counter/counter_test.go
@@ -0,0 +1,24 @@
+package counter
+
+import (
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	counterSafe := New(3)
+	if have, want := counterSafe.max, 3; have != want {
+		t.Errorf("Init val of counterSafe is wrong. Have: %d, want: %d.", have, want)
+	}	
+
+	id1 := counterSafe.Inc("test")
+
+	if have, want := id1, 1; have != want {
+		t.Errorf("Inc of counter failed. Have: %d, want: %d.", have, want)
+	}
+	
+	id2 := counterSafe.Inc("test")
+
+	if have, want := id2, 2; have != want {
+		t.Errorf("Inc of counter failed. Have: %d, want: %d.", have, want)
+	}		
+}
